2499271f3710dc94d0c7399a50e6b175318893cd
fady malak <fady.malak.awad@gmail.com>
2022-03-03 17:10:03 +0200

    NewBranch Created

---
+++ myapi/models2.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class BaseItem(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(blank=False)
+    image = models.ImageField()
+    class Meta:
+        abstract = True
+
+class MSQ(BaseItem):
+    choose = models.JSONField()
+    correct = models.TextField(blank=False)
+
+
+class YNQ(BaseItem):
+    CHOICES = (("T","True"),("T","False"))
+
+    correct = models.CharField(max_length=1,choices=CHOICES)
+
+class GQ(BaseItem):
+    answer = models.TextField(blank=False)
